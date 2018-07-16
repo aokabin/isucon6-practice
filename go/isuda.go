9c5f1dbba1a1cf629dd67249586a18686207d2c5
aokabin <info.aokabin@gmail.com>
2018-07-16 14:19:57 +0900

    Update

--- go/isuda.go
+++ go/isuda.go
@@ -257,7 +257,7 @@ func keywordByKeywordHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	keyword := mux.Vars(r)["keyword"]
-	row := db.QueryRow(`SELECT * FROM entry WHERE keyword = ?`, keyword)
+	row := db.QueryRow(`SELECT * FROM entry WHERE keyword = '?'`, keyword)
 	e := Entry{}
 	err := row.Scan(&e.ID, &e.AuthorID, &e.Keyword, &e.Description, &e.UpdatedAt, &e.CreatedAt)
 	if err == sql.ErrNoRows {
